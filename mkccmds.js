d9adbb7c196587afb59d442309012c053ad38f78
Parker Snell <parkersnell@gmail.com>
2019-08-07 09:22:04 -0700

    compile_commands.json creation script

---
+++ mkccmds.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const json = fs
+  .readdirSync(__dirname)
+  .filter(x => x.endsWith('.cpp'))
+  .map(name => ({
+    directory: __dirname.replace(/\\/g, '/'),
+    command: `cl.exe /nologo /DWIN32 /D_WINDOWS /W3 /EHsc /MDd /Zi /Ob0 /Od /RTC1 -std:c++17 ${name}`,
+    file: `${__dirname.replace(/\\/g, '/')}/${name}`
+  }))
+
+fs.writeFileSync(
+  __dirname + '/compile_commands.json',
+  JSON.stringify(json),
+  { encoding: 'utf-8' }
+);
