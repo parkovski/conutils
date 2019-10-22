4a8fa8ffe07133d0c045df7c5f9779c9db1c4bbe
Parker Snell <parkersnell@gmail.com>
2019-10-22 18:43:35 -0400

    add NTSTATUS support

--- hresult.cpp
+++ hresult.cpp
@@ -1,13 +1,33 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <winternl.h>
 
 #include <iostream>
 #include <sstream>
 
+#define REQUIRE_ARGS(_n) if (argc != (_n) + 1) { \
+    std::cout << "Expected " << (_n) << "arguments.\n"; return 1; \
+  } else { \
+    argv[1] = argv[_n]; \
+  }
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cerr << "Usage: hresult <number>\n";
+    std::cerr << "Usage: hresult [nt] <number>.\n";
+    std::cerr << "The nt option interprets NTSTATUS values.\n";
+    std::cerr << "Numbers can be in decimal or hex (0x...) format.\n";
     return 1;
+  }
+
+  constexpr int cvt_none = 0;
+  constexpr int cvt_nt = 1;
+
+  int convert = cvt_none;
+  if (!_strcmpi(argv[1], "nt")) {
+    convert = cvt_nt;
+    REQUIRE_ARGS(2)
+  } else {
+    REQUIRE_ARGS(1)
   }
 
   std::stringstream ss;
@@ -19,10 +39,32 @@ int main(int argc, char *argv[]) {
   }
   ss >> messageId;
 
+  std::cout << "Code 0x" << std::hex << std::uppercase << messageId
+    << std::dec << std::nouppercase << " (" << messageId << ")\n";
+
+  switch (convert) {
+    case cvt_nt:
+      messageId = reinterpret_cast<ULONG (*)(NTSTATUS)>(
+          GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlNtStatusToDosError")
+        )(messageId);
+      std::cout << "Converted from NTSTATUS = 0x" << std::hex << std::uppercase
+        << messageId << std::dec << std::nouppercase << " (" << messageId << ")\n";
+      break;
+    default:
+      break;
+  }
+
   char buffer[1024];
 
-  FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                 nullptr, messageId, 0, buffer, 1024, nullptr);
+  auto len = FormatMessageA(
+    FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+    nullptr, messageId, 0, buffer, 1024, nullptr
+  );
+
+  if (!len) {
+    std::cout << "Couldn't decode result code.\n";
+    return GetLastError();
+  }
 
   std::cout << buffer;
 
